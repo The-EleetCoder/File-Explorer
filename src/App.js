34666079fdb1c975ada0d9d77fd3177bc07a8562
The-EleetCoder <jaijain1803@gmail.com>
2024-03-27 15:36:34 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import explorer from "./data/folderData";
+import Folder from "./components/Folder";
+
 function App() {
-  return <div>Hi</div>;
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  return (
+    <div>
+      <Folder explorer={explorerData} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Folder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Folder = ({explorer}) => {
+
+  return (
+    <div>
+        {explorer.name}
+    </div>
+  )
+}
+
+export default Folder
