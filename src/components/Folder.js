e044cabc9e43e1243181bd3b07ee618f6d2f8d23
The-EleetCoder <jaijain1803@gmail.com>
2024-03-28 13:52:00 +0530

    added buttons

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -2,6 +2,27 @@ import React, { useState } from "react";
 
 const Folder = ({ explorer }) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  // when we click on add button, it should not just expand
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+
+  // handler for adding new folder or file
+  const onAddFolder = ((e)=>{
+    if (e.keyCode === 13 && e.target.value){
+      setShowInput({...showInput, visible: false});
+    }
+  })
 
   // checking if its a folder or file
   if (explorer.isFolder) {
@@ -13,13 +34,27 @@ const Folder = ({ explorer }) => {
 
           {/* buttons */}
           <div className="button-div">
-            <button>📁+</button>
-            <button>📄+</button>
+            <button onClick={(e) => handleNewFolder(e, true)}>📁+</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>📄+</button>
           </div>
         </div>
 
-        {/* iterating through folder items */}
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {/* adding new folder/file */}
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                className="inputContainer-input"
+                autoFocus
+                type="text"
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                onKeyDown={(e)=>onAddFolder(e)}
+              />
+            </div>
+          )}
+
+          {/* iterating through folder items */}
           {explorer.items.map((exp) => {
             return <Folder explorer={exp} key={exp.id} />;
           })}
